ae7b6006e2f64c5bb8f32ca79025dbccb57d1210
Vishal Jaiswal <Vishal.Jaiswal@acs.org.au>
2025-02-19 11:23:22 +1100

    feat: initial commit of storybook with jsx

--- ui.frontend/.eslintrc.js
+++ ui.frontend/.eslintrc.js
@@ -1,8 +1,6 @@
 module.exports =  {
     parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-    extends:  [
-      'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    ],
+    extends:  ["plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
     parserOptions:  {
       ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
       sourceType:  'module',  // Allows for the use of imports
